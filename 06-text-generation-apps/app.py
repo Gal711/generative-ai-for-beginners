6179bc4f7c2d8f0a1db07433d61abcc6dd4044b3
Gal711 <gaelbrunel@hotmail.fr>
2023-11-12 23:50:32 +0100

    lesson 6

--- 06-text-generation-apps/app.py
+++ 06-text-generation-apps/app.py
@@ -1,27 +1,36 @@
-import openai
 import os
-import dotenv
+from openai import OpenAI
+from dotenv import load_dotenv, find_dotenv
 
-# import dotenv
-dotenv.load_dotenv()
+load_dotenv(find_dotenv())
 
-openai.api_key = os.getenv("API_KEY") 
+# Note that we can set different env variables to different OPENAI keys and just map the right one to openai.api_key here
+# Example: have both OPENAI_API_KEY (for OpenAI) and AOAI_API_KEY (for Azure OpenAI) as options 
+# openai.api_key  = os.getenv('OPENAI_API_KEY')
+
+
+
+client = OpenAI(api_key=os.getenv('OPENAI_API_KEY'))
+
+# dotenv.load_dotenv()
+
+# openai.api_key = os.getenv("OPENAI_API_KEY") 
 
 # enable below if you use Azure Open AI
-openai.api_type = 'azure' 
-openai.api_version = '2023-05-15'
-openai.api_base = os.getenv("API_BASE")
+# openai.api_type = 'azure' 
+# openai.api_version = '2023-05-15'
+# openai.api_base = os.getenv("API_BASE")
 
 # add your completion code
 prompt = "Complete the following: Once upon a time there was a"
 
 # engine
-engine = "davinci-001"
+engine = "davinci-002"
 
 # deployment_id, azure specific
 deployment_name = os.getenv("DEPLOYMENT_NAME")
 
-completion = openai.Completion.create(engine=deployment_name, prompt=prompt, max_tokens=600)
+completion = client.completions.create(model=engine, prompt=prompt, max_tokens=100)
 
 # print response
 print(completion.choices[0].text)
