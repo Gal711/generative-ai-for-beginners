b0a07373650e62eafaf2c52027c141cf30b13937
Gal711 <gaelbrunel@hotmail.fr>
2023-11-14 22:57:42 +0100

    lesson 9

--- 09-building-image-applications/app.py
+++ 09-building-image-applications/app.py
@@ -1,4 +1,5 @@
 import openai
+from openai import OpenAI
 import os
 import requests
 from PIL import Image
@@ -6,23 +7,31 @@ import dotenv
 
 # import dotenv
 dotenv.load_dotenv()
+client = OpenAI(api_key=os.getenv('OPENAI_API_KEY'))
 
 # Get endpoint and key from environment variables
-openai.api_base = os.environ['AZURE_OPENAI_ENDPOINT']
-openai.api_key = os.environ['AZURE_OPENAI_KEY']     
+# openai.api_base = os.environ['AZURE_OPENAI_ENDPOINT']
+# openai.api_key = os.environ['AZURE_OPENAI_KEY']     
 
-# Assign the API version (DALL-E is currently supported for the 2023-06-01-preview API version only)
-openai.api_version = '2023-06-01-preview'
-openai.api_type = 'azure'
+# # Assign the API version (DALL-E is currently supported for the 2023-06-01-preview API version only)
+# openai.api_version = '2023-06-01-preview'
+# openai.api_type = 'azure'
 
 
+# response = client.images.generate(
+#   model="dall-e-3",
+#   prompt="a white siamese cat",
+#   size="1024x1024",
+#   quality="standard",
+#   n=1,
+# )
 try:
     # Create an image by using the image generation API
-    generation_response = openai.Image.create(
+    generation_response = client.images.generate(
+        model="dall-e-3",
         prompt='Bunny on horse, holding a lollipop, on a foggy meadow where it grows daffodils',    # Enter your prompt text here
         size='1024x1024',
-        n=2,
-        temperature=0,
+        n=1,
     )
     # Set the directory for the stored image
     image_dir = os.path.join(os.curdir, 'images')
@@ -35,7 +44,7 @@ try:
     image_path = os.path.join(image_dir, 'generated-image.png')
 
     # Retrieve the generated image
-    image_url = generation_response["data"][0]["url"]  # extract image URL from response
+    image_url = generation_response.data[0].url  # extract image URL from response
     generated_image = requests.get(image_url).content  # download the image
     with open(image_path, "wb") as image_file:
         image_file.write(generated_image)
@@ -43,14 +52,13 @@ try:
     # Display the image in the default image viewer
     image = Image.open(image_path)
     image.show()
-
 # catch exceptions
-except openai.error.InvalidRequestError as err:
+except openai.OpenAIError as err:
     print(err)
 
 # ---creating variation below---
 
-response = openai.Image.create_variation(
+response = client.images.create_variation(
   image=open(image_path, "rb"),
   n=1,
   size="1024x1024"
@@ -58,7 +66,7 @@ response = openai.Image.create_variation(
 
 image_path = os.path.join(image_dir, 'generated_variation.png')
 
-image_url = response['data'][0]['url']
+image_url = response.data[0].url
 
 generated_image = requests.get(image_url).content  # download the image
 with open(image_path, "wb") as image_file:
